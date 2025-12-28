a7f077552c4ac60395d52554ac28fe443126203f
Bijay Prasad <bijaylife2001@gmail.com>
2025-12-28 15:32:41 +0530

    health route

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
 app.use("/ingest", ingestRoutes);
 app.use("/aggregates", aggregateRoutes);
 
